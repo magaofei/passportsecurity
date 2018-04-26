5696fae28ef5b755d270d2ae43381fe4fbf72513
Mark <mamian521@gmail.com>
2018-04-26 22:40:11 +0800

    init

---
+++ src/main/java/com/magaofei/passportsecurity/PassportSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.magaofei.passportsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassportSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassportSecurityApplication.class, args);
+    }
+}
